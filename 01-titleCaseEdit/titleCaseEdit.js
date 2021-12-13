bed31c6430ba55ca91e6d5b0705d7418d7adef43
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2021-12-13 09:53:58 -0500

    add function body

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,9 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+ 
+  let tStr = title.toString().replaceAll(/\b\w/g, function(a) { return a.toUpperCase(); });
+ 
+  return tStr;
+  
 }
-
 // Do not edit this line;
 module.exports = titleCaseEdit;
