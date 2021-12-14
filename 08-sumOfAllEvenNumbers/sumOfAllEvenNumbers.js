0515245c80cb20456659dae0f2190a4d9e367ba3
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2021-12-13 20:54:30 -0500

    modified:   sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let val = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0)
+      val++;
+  }
+
+  return val;
 }
 
 // Do not edit this line;
