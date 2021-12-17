b0dac11a7ce24e3f8f8452fd821aaf5963dc7079
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2021-12-17 10:41:37 -0500

    too many else ifs

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,16 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let arr = [];
+  for (start; start <= end; start++) {
+    if (start % 3 == 0 && start % 5 == 0)
+      arr.push("FizzBuzz");
+    else if (start % 3 == 0)
+      arr.push("Fizz");
+    else if (start % 5 == 0)
+      arr.push("Buzz");
+    else
+      arr.push(start);
+  }
+  return arr;
 }
 
 // Do not edit this line;
