cb8fe29dc3eae54bc11bcb26baf54aaa7b6b98f8
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2021-12-17 17:31:52 -0500

    modified:   setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,9 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  const union = new Set(); //I love set theory!!
+  for (const i of args)
+    for (const j of i)
+      union.add(j);
+  return union;
 }
 
 // Do not edit this line;
